831fff2aed5a664d912d629469cb1ca79da15f8b
Subhan <k1subhank@gmail.com>
2024-02-03 00:49:24 +0500

    added more

---
+++ import.js
@@ -0,0 +1,4 @@
+const randomSk = require("./random.js");
+let u = randomSk({name: "subhan", age: 13,hobby : "coding",0:3,true:34},true);
+console.log(u);
+
--- random.js
+++ random.js
@@ -1,5 +1,24 @@
-const randomSk = (arrOrString) => {
-    let a = arrOrString.length;
-    let randArr = Math.floor(Math.random()*a);
-    return arrOrString[randArr];
-};module.exports = randomSk;
+const randomSk = (arrOrString, valOrKeyIfObjectTrueForValFalForKeys) => {
+    if (Array.isArray(arrOrString)) {
+        let a = arrOrString.length;
+        let randArr = Math.floor(Math.random() * a);
+        return arrOrString[randArr];
+    } else if (
+        typeof arrOrString === 'object' &&
+        !Array.isArray(arrOrString) &&
+        arrOrString !== null
+    ) {
+        if (valOrKeyIfObjectTrueForValFalForKeys) {
+            let vals = Object.values(arrOrString);
+            let randomVals = Math.floor(Math.random() * vals.length);
+            return vals[randomVals]
+        } else {
+            // Handle the case when valOrKeyIfObjectTrueForValFalForKeys is false
+            // You can customize this return statement based on your requirements
+            return 'ObjectHandlingWhenFalse';
+        }
+    }
+};
+
+module.exports = randomSk;
+
